73ff963e85aa1ef50ad5234215dcba5b55560773
not-adi <yadav.adi0718@gmail.com>
2025-01-16 19:01:14 +0530

    Added hollow rectangle

---
+++ hollowrectangle.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+int main() {
+    int i,j,n;
+    std::cout<<"Enter the number of *'s to be printed in length: ";
+    std::cin>>n;
+    for(i=0;i<n;i++) {
+        for(j=0;j<n;j++) {
+        if (i==0 || i==n-1 || j==0  || j==n-1) {
+            std::cout<<" *";
+        } else {
+            std::cout<<"  ";
+        } 
+    }
+        std::cout<<"\n";
+    }
+
+    return 0;
+}
