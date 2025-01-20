31094900584062334d7a9f21bdde893596d2c6f1
not-adi <yadav.adi0718@gmail.com>
2025-01-20 18:23:59 +0530

    Minor fix

--- hollowrectangle.cpp
+++ hollowrectangle.cpp
@@ -7,7 +7,7 @@ int main() {
         for(j=0;j<n;j++) {
         if (i==0 || i==n-1 || j==0  || j==n-1) {
             std::cout<<" *";
-        } else {
+        } else { 
             std::cout<<"  ";
         } 
     }
